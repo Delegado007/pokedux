260468c6b6a490b88cc4c0b945703caf13a73a0f
Delegado <maxharper2013@gmail.com>
2022-07-25 21:12:58 -0300

    [FIX] bad import

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
@@ -55,56 +52,6 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html'
     }),
-    // new WebpackPwaManifestPlugin({
-    //   publicPath: "/",
-    //   name: 'Petgram - Tu web de fotos para mascotas',
-    //   short_name: 'Petgram',
-    //   start_url: 'https://petgram-delegado.netlify.app',
-    //   display: 'fullscreen',
-    //   crossorigin: null,
-    //   prefer_related_applications: true,
-    //   description: 'Con petgram puedes encontrar fotos de animales muy bonitos',
-    //   background_color: '#fff',
-    //   theme_color: '#b1a',
-    //   icons: [
-    //     {
-    //       src: path.resolve('src/assets/icon.png'),
-    //       sizes: [96, 128, 192, 256, 384, 512]
-    //     },
-    //     {
-    //       src: path.resolve('src/assets/touch-icon.png'),
-    //       size: '180x180',
-    //       type: 'image/png',
-    //       purpose: 'maskable'
-    //     }
-    //   ]
-    // }),
-    // new WorkboxWebpackPlugin.GenerateSW({
-    //   runtimeCaching: [
-    //     {
-    //       urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
-    //       handler: 'CacheFirst',
-    //       options: {
-    //         cacheName: 'images'
-    //       }
-    //     },
-    //     {
-    //       urlPattern: new RegExp('https://api-petgram-875dq86v1-delegado007.vercel.app'),
-    //       handler: 'NetworkFirst',
-    //       options: {
-    //         cacheName: 'api'
-    //       }
-    //     }
-    //   ]
-    // }),
-    // new CopyPlugin({
-    //   patterns: [
-    //     {
-    //       from: path.resolve(__dirname, "src", "assets"),
-    //       to: "assets"
-    //     }
-    //   ]
-    // }),
   ],
   optimization: {
     minimize: true,
